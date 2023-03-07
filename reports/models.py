503259e1ea0b4d4bd7b8e13d809c8a65d6045360
vimalkvcandorcode <vimal@candorcode.com>
2023-03-07 10:25:31 +0530

    changes

--- reports/models.py
+++ reports/models.py
@@ -4,7 +4,7 @@ from django.db import models
 from django.contrib.auth import get_user_model
 from django.forms import IntegerField
 
-User = get_user_model()
+User = get_user_model() 
 
 # Create your models here.
 
